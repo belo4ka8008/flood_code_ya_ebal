837db21a21d3352f832a65cad9244cceb2d9f6c5
belo4ka8008 <dmitry_bessmertnyy@mail.ru>
2024-08-12 19:22:59 +0300

    Add files via upload
    мое почтение

---
+++ ЛП_ВС.py
@@ -0,0 +1,21 @@
+#loop back
+loop=1
+while (loop<10):
+#questions
+
+ name = input('Назовите существительное во множественном числе: ')
+ rhyme_name = input('Назоваите рифму к предыдущему существительному : ')
+ verb = input('Выберите глагол: ')
+ relative_male = input('родственник мужского пола: ')
+ relative_female =input('родственник женского пола: ')
+ adjective = input('прилагательное: ')
+ noun3 = input('секс игрушка: ')
+#story
+ print('---------------------')
+ print(name,' части, солнца зари')
+ print('рубят злые '+rhyme_name+', сжигают внутри')
+ print(verb+' выше неба '+relative_male+' и '+relative_female)
+ print(adjective+' искры - , брызги'+noun3)
+ print('---------------------')
+
+loop = loop+1
